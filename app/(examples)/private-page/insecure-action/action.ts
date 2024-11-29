6700063e9083dd563b2fc48d8d8a43f34a0dcee4
Rai Siqueira <rai93siqueira@gmail.com>
2024-11-29 14:55:07 -0300

    update insecure action page
    Signed-off-by: Rai Siqueira <rai93siqueira@gmail.com>

--- app/(examples)/private-page/insecure-action/action.ts
+++ app/(examples)/private-page/insecure-action/action.ts
@@ -2,18 +2,13 @@
 
 import { z } from 'zod'
 
-import { actionClient } from '../../../../lib/safe-action'
-
 const schema = z.object({
   userId: z.number(),
 })
 
-export const deleteUser = actionClient
-  .schema(schema)
-  .metadata({ actionName: 'deleteUser' })
-  .action(async ({ parsedInput: { userId } }) => {
-    console.log(`Deleting user with id: ${userId}`)
-    await new Promise((res) => setTimeout(res, 1000))
-    console.log(`User ${userId} deleted`)
-    return true
-  })
+export const deleteUser = async (csrfToken: string, data: z.infer<typeof schema>) => {
+  console.log(`Deleting user with id: ${data.userId}`)
+  await new Promise((res) => setTimeout(res, 1000))
+  console.log(`User ${data.userId} deleted`)
+  return true
+}
--- app/(examples)/private-page/insecure-action/delete-user.tsx
+++ app/(examples)/private-page/insecure-action/delete-user.tsx
@@ -4,10 +4,14 @@ import { deleteUser } from './action'
 
 const getUserId = () => 1993
 
-export function DeleteUser() {
+type DeleteUserProps = {
+  csrfToken: string
+}
+
+export function DeleteUser({ csrfToken }: DeleteUserProps) {
   const userId = getUserId()
   const handleDelete = async () => {
-    await deleteUser({ userId })
+    await deleteUser(csrfToken, { userId })
   }
   return (
     <button className="bg-blue-400 text-white p-2 rounded-md" onClick={handleDelete}>
